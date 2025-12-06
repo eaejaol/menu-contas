5b621e4b60e8d9deae4af7b9ce30b420a86b25f1
João Costa <joaolucas0705@gmail.com>
2025-12-06 10:00:06 -0400

    Primeiro envio do projeto menu contas

---
+++ script.js
@@ -0,0 +1,7 @@
+// nada
+document.getElementById("lista-pagamentos").innerHTML = "";
+
+/* Função para abrir o Trabalho emilly */
+function abrirTrabalho(caminho) {
+    window.location.href = caminho;
+}
